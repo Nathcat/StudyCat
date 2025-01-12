bcff28b056c887fcc890e0ea4c8cf58e209031c7
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-01-12 16:01:42 +0000

    Init base project

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>StudyCat</title>
+
+        <link rel="stylesheet" href="https://nathcat.net/static/css/new-common.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <div id="page-content" class="content">
+            <?php include("header.php"); ?>
+
+            <div class="main">
+
+            </div>
+
+            <?php include("footer.php"); ?>
+        </div>
+    </body>
+</html>
